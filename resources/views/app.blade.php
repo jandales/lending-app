8c7d98f156a4b71038d4da2ea3d70fb63bb3b0c3
jandales <jesusandales021993@gmail.com>
2022-08-08 23:46:33 +0800

    updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,24 +9,18 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
         <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
+
         @production
             @php
-                $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-               
+                $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);               
             @endphp
              <link rel="stylesheet" href="{{asset("build/".$manifest['resources/css/app.css']['file'])}}" >
-             <script type="module" src="{{asset("build/".$manifest['resources/js/app.js']['file'])}}" ></script>
-      
-                
-        @else
-                
-        @vite(['resources/css/app.css', 'resources/js/app.js']);
+             <script type="module" src="{{asset("build/".$manifest['resources/js/app.js']['file'])}}" ></script>           
+        @else 
+            @vite(['resources/css/app.css','resources/js/app.js'])
         @endproduction
-        @php
+
   
-        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true)
-       
-    @endphp
     </head>
     <style>
         body{
