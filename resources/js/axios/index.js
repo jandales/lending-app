f9f67b4e782b0b8ddd22c01739d9eb216578837f
Jess Andales <46065062+jandales@users.noreply.github.com>
2022-08-10 20:15:19 +0800

    Update index.js

--- resources/js/axios/index.js
+++ resources/js/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
     //baseURL: 'http://127.0.0.1:8000/api',
-    baseURL : 'https://codastore.herokuapp.com/api',
+    baseURL : 'https://etto-lending-app.herokuapp.com/api',
     withCredentials: true,
 });
 
