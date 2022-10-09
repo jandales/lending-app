b54b34dc5ba53bdcf0d5f5d39d46db2671a742e0
jandales <jesusandales021993@gmail.com>
2022-10-09 21:09:11 +0800

    apply lazy load on components

--- resources/js/route/index.js
+++ resources/js/route/index.js
@@ -1,33 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Login from '../pages/Login.vue'
-import Home from '../pages/Home.vue'
-import LoanTypeIndex from '../components/LoanTypes/Index.vue'
-import CreateType from '../components/LoanTypes/Create.vue'
-import EditType from '../components/LoanTypes/Edit.vue'
-import borrowerIndex from '../components/borrowers/Index.vue'
-import borrowerCreate from '../components/borrowers/Create.vue'
-import borrowerEdit from '../components/borrowers/Edit.vue'
-import borrowerDetails from '../components/borrowers/Details.vue';
-import Dashboard from '../pages/Dashboard.vue'
-import ForgotPassword from '../pages/ForgotPassword.vue'
-import ResetPassword  from '../pages/ResetPassword.vue'
-import Page404 from '../pages/Page404.vue'
-import Setting from '../pages/Setting.vue'
+
+
+
+
+
+
+
 import ChangePassword from '../components/settings/ChangePassword.vue'
-import Profile from '../components/settings/Profile.vue'
-import LoanIndex from '../components/loans/Index.vue';
-import LoanCreate from '../components/loans/Create.vue';
-import PaymentIndex from '../components/payments/Index.vue';
-import PaymentCreate from '../components/payments/Create.vue';
-import LoanDetails from '../components/loans/Details.vue';
-import ReportLoans from '../components/reports/ReportLoans.vue';
-import ReportBorrower from '../components/reports/ReportBorrower.vue';
-import ReportPayments from '../components/reports/ReportPayments.vue';
-import Interest from '../components/settings/Interest.vue';
-import UserIndex from '../components/users/Index.vue';
-import UserCreate from '../components/users/Create.vue';
-import UserEdit from '../components/users/Edit.vue';
 import middleware  from '../middleware/index';
 
 const router = createRouter({ 
@@ -37,66 +17,66 @@ const router = createRouter({
     routes : [
                 
         { 
-            path : '/', name : 'home', component : Home, beforeEnter : middleware.authenticated , children : 
+            path : '/', name : 'home', component : () => import('../pages/Home.vue'), beforeEnter : middleware.authenticated , children : 
             [
-                { path : '' , name : 'dashboard', component : Dashboard },
+                { path : '' , name : 'dashboard', component : () => import('../pages/Dashboard.vue') },
 
-                { path : 'loans' , name : 'loans', component : LoanIndex },
+                { path : 'loans' , name : 'loans', component : () => import('../components/loans/Index.vue') },
 
-                { path : 'loans/details/:id' , name : 'loans.details', component : LoanDetails },
+                { path : 'loans/details/:id' , name : 'loans.details', component : () => import('../components/loans/Details.vue') },
 
-                { path : 'loans/create' , name : 'loans.create', component : LoanCreate },
+                { path : 'loans/create' , name : 'loans.create', component : () => import('../components/loans/Create.vue') },
 
-                { path : 'loan-types' , name : 'loanTypes', component : LoanTypeIndex },
+                { path : 'loan-types' , name : 'loanTypes', component : () => import('../components/LoanTypes/Index.vue') },
 
-                { path : 'loan-types/create' , name : 'loanTypes.create',  component : CreateType  },
+                { path : 'loan-types/create' , name : 'loanTypes.create',  component : () => import('../components/LoanTypes/Create.vue')  },
 
-                { path : 'loan-types/edit/:id', name : 'loanTypes.edit',  component : EditType  },
+                { path : 'loan-types/edit/:id', name : 'loanTypes.edit',  component : () => import('../components/LoanTypes/Edit.vue')  },
 
-                { path : 'borrowers', name : 'borrowers', component : borrowerIndex },
+                { path : 'borrowers', name : 'borrowers', component : () => import('../components/borrowers/Index.vue') },
 
-                { path : 'borrowers/create', name : 'borrowers.create', component : borrowerCreate },
+                { path : 'borrowers/create', name : 'borrowers.create', component : () => import('../components/borrowers/Create.vue') },
 
-                { path : 'borrowers/edit/:id', name : 'borrowers.edit', component : borrowerEdit },
+                { path : 'borrowers/edit/:id', name : 'borrowers.edit', component : () => import('../components/borrowers/Edit.vue') },
 
-                { path : 'borrowers/details/:id', name : 'borrowers.details', component : borrowerDetails },            
+                { path : 'borrowers/details/:id', name : 'borrowers.details', component :  () => import('../components/borrowers/Details.vue') },            
 
-                { path: 'payments' , name : 'payments', component : PaymentIndex },
-
-                { path: 'payments/create/:loan_id?', name : 'payments.create', component : PaymentCreate },
-
-                { path: 'reports/loans', name : 'report.loans', component : ReportLoans },
-
-                { path: 'reports/payments', name : 'report.payments', component : ReportPayments },
-
-                { path : 'reports/customers', name : 'report.customers', component : ReportBorrower },
-
-                { path : 'users', name : 'users', component : UserIndex,  beforeEnter : middleware.role  },
-
-                { path : 'users/create', name : 'users.create', component : UserCreate,  beforeEnter : middleware.role },
-
-                { path : 'users/edit/:id', name : 'users.edit', component : UserEdit,  beforeEnter : middleware.role  },
-
-                { path : 'settings', name: 'settings', component : Setting,  children : [
+                { path: 'payments' , name : 'payments', component : () => import('../components/payments/Index.vue')},
       
-                    {path : 'profile', name : 'settings.profile', component : Profile},
+                { path: 'payments/create/:loan_id?', name : 'payments.create', component : () => import('../components/payments/Create.vue') },
+
+                { path: 'reports/loans', name : 'report.loans', component : () => import('../components/reports/ReportLoans.vue') },
+
+                { path: 'reports/payments', name : 'report.payments', component : () => import('../components/reports/ReportPayments.vue') },
+
+                { path : 'reports/customers', name : 'report.customers', component :  () => import('../components/reports/ReportBorrower.vue') },
+
+                { path : 'users', name : 'users', component : () => import('../components/users/Index.vue'),  beforeEnter : middleware.role  },
+
+                { path : 'users/create', name : 'users.create', component : () => import('../components/users/Create.vue'),  beforeEnter : middleware.role },
+
+                { path : 'users/edit/:id', name : 'users.edit', component : () => import('../components/users/Edit.vue'),  beforeEnter : middleware.role  },
+
+                { path : 'settings', name: 'settings', component : () => import('../pages/Setting.vue'),  children : [
+      
+                    {path : 'profile', name : 'settings.profile', component : () => import('../components/settings/Profile.vue')},
 
                     // {path : 'change-password', name : 'settings.change.password', component :ChangePassword},
 
-                    {path : 'interest', name : 'settings.interest', component : Interest, beforeEnter : middleware.role}
+                    {path : 'interest', name : 'settings.interest', component : ()=> import('../components/settings/Interest.vue'), beforeEnter : middleware.role}
 
                 ], beforeEnter : middleware.authenticated},
                                
             ]
         },
 
-        { path : '/login', name : 'login', component : Login, beforeEnter : middleware.guest },
+        { path : '/login', name : 'login', component : () => import('../pages/Login.vue'), beforeEnter : middleware.guest },
 
-        { path : '/forgot-password', name : 'forgot.password', component : ForgotPassword, beforeEnter : middleware.guest },
+        { path : '/forgot-password', name : 'forgot.password', component : () => import('../pages/ForgotPassword.vue'), beforeEnter : middleware.guest },
 
-        { path : '/reset-password/:token', name : 'reset.password', component : ResetPassword, beforeEnter : middleware.guest },       
+        { path : '/reset-password/:token', name : 'reset.password', component : () => import('../pages/ResetPassword.vue'), beforeEnter : middleware.guest },       
 
-        { path : '/page-not-found', name : 'page.not.found', component : Page404 },
+        { path : '/page-not-found', name : 'page.not.found', component : () => import('../pages/Page404.vue') },
        
     ],
     linkActiveClass: "active"
